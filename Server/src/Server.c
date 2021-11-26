300a15e702bbd4076b7c596b49c4bff0a2a54eb5
zSavT <verioc3@gmail.com>
2021-11-26 10:54:58 +0100

    initial basis

---
+++ Server/src/Server.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Server.c
+ Author      : Francesco Saverio Cassano
+ Version     : 0.1
+ Copyright   : Free to use, just notice me.
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Test"); /* prints Test */
+	return EXIT_SUCCESS;
+}
