0cb59505a9b7e4cb22e3d7bace46684e8b2c7364
zSavT <francesco.saverio.cassano@gmail.com>
2026-03-27 19:15:16 +0100

    Update

--- workspace/Client/src/Client.c
+++ workspace/Client/src/Client.c
@@ -19,30 +19,36 @@ int main(int argc, char *argv[]) {
     int result = WSAStartup(MAKEWORD(2, 2), &wsa_data);
     if (result != NO_ERROR) {
       errorHandler("Error at WSAStartup()\n");
-      return 0;
+      return WSA_STARTUP_FAILURE;
     }
   #endif
 
   // Socket creation
   int my_socket = 0;
-  my_socket = socketCreation(my_socket);
-  if (my_socket < 0) {
-    errorHandler("Socket creation failed.\n");
-    closeAndCleanSocket(&my_socket);
-    return -1;
+  if (socketCreation(&my_socket) != APP_SUCCESS) {
+	clearWinsock();
+    return SOCKET_CREATION_FAILURE;
   }
 
   // Construction of the server andress
-  struct sockaddr_in sad = constructionServerAddress(argc, argv);
+  struct sockaddr_in sad;
+  if(constructionServerAddress(argc, argv, &sad) != APP_SUCCESS) {
+	  closeAndCleanSocket(&my_socket);
+	  return INVALID_PORT;
+  }
 
   // Connection of the server
-  connectionToServer(my_socket, sad);
-  operationChoice(my_socket);
+  if(connectionToServer(my_socket, sad) != APP_SUCCESS) {
+	  closeAndCleanSocket(&my_socket);
+	  return SOCKET_CONNECT_FAILURE;
+  }
+  
+  AppErrorCode op_result = operationChoice(my_socket);
 
   closeAndCleanSocket(&my_socket);
 
   printf("\n");
   system("pause");
-  return 0;
+  return op_result;
 
 } // main end
